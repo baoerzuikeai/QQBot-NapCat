77cd1f886bf4893daa97ab583cceb2757af1f8f0
baoerzuikeai <2634174807@qq.com>
2025-07-06 02:33:19 +0800

    test send private_msg

--- internal/transport/websocket/handler.go
+++ internal/transport/websocket/handler.go
@@ -1,8 +1,11 @@
 package websocket
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
@@ -32,6 +35,46 @@ func HandlerWebsocket(w http.ResponseWriter, r *http.Request) {
 			break // Exit on error
 		}
 		log.Printf("Received : %s", string(msg))
-
+		setStatusCode(w, 10)
 	}
+}
+
+func setStatusCode(w http.ResponseWriter, code int) {
+	url := "http://192.168.2.190:3000/send_private_msg"
+	method := "POST"
+
+	payload := strings.NewReader(`{
+    "user_id": "2634174807",
+    "message": [
+        {
+            "type": "text",
+            "data": {
+                "text": "napcat"
+            }
+        }
+    ]
+}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
